99b6987667a909a19492ac5d7abb53ec974b3ce4
권용민 <briandr@naver.com>
2022-02-14 20:11:58 +0900

    프로젝트 생성

---
+++ app/src/main/java/org/sopt/study/catholiclibraryseat/MainActivity.kt
@@ -0,0 +1,11 @@
+package org.sopt.study.catholiclibraryseat
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
