5abd45e013432b8579de2480cc39b01060400705
권용민 <briandr@naver.com>
2022-02-17 03:37:18 +0900

    메인 뷰 진짜 고민 중..

--- app/src/main/java/org/sopt/study/catholiclibraryseat/ui/MainActivity.kt
+++ app/src/main/java/org/sopt/study/catholiclibraryseat/ui/MainActivity.kt
@@ -3,10 +3,14 @@ package org.sopt.study.catholiclibraryseat.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import org.sopt.study.catholiclibraryseat.R
+import org.sopt.study.catholiclibraryseat.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
     }
 }
